358c3ff3bf37a3917c9caa25815f3926ad27c0a8
Diego Yanez <diegosyanez92@gmail.com>
2018-04-09 17:00:45 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :school, :roster, :grade
+  attr_reader
+
+  def initialize(school)
+    @school= school
+    @roster = {}
+  end
+
+  def add_student (name, grade)
+    unless @roster.include? (grade)
+      @roster[grade] = []
+    end
+    @roster[grade]<< name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  
+end
